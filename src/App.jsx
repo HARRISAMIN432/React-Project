79d606239ff024b48454726dcbe5242715fd07ce
Harris <harrisaminjutt@gmail.com>
2025-02-15 15:57:46 +0500

    Component Props

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,35 @@ import SearchIcon from "./svgs/SearchIcon";
 import RotatedSquare from "./svgs/RotatedSquare";
 import "./App.css";
 import Row from "./components/common/Row";
+import UserIcon from "./svgs/UserIcon";
+import Cart from "./svgs/BucketIcon";
 
 function App() {
   return (
-    <div className="w-full bg-[#1e28320d] h-[60px]">
+    <div className="w-full bg-[rgba(79,95,112,0.05)] h-[60px]">
       <div className="w-full px-[10%] h-full">
-        <div className="flex items-center h-full">
-          <div className="flex items-center">
-            <SearchIcon />
+        <Row className="flex items-center h-full justify-between">
+          <SearchIcon />
+          <Row className="items-center">
             <RotatedSquare />
             <h1 className="text-[28px] text-black mx-[10px]">RiftCom</h1>
             <RotatedSquare />
-          </div>
-          <Row />
-        </div>
+          </Row>
+          <Row>
+            <Row>
+              <UserIcon />
+              <p className="text=[#le2832] hover:text-slate-400 ml-[2px]">
+                Account
+              </p>
+            </Row>
+            <Row className="ml-[20px]">
+              <Cart />
+              <p className="text=[#le2832] hover:text-slate-400 ml-[2px]">
+                Cart
+              </p>
+            </Row>
+          </Row>
+        </Row>
       </div>
     </div>
   );
--- src/components/common/Row.jsx
+++ src/components/common/Row.jsx
@@ -2,9 +2,12 @@ import React, { Component } from "react";
 
 // Class component
 class Row extends Component {
-  state = { title: "This is a row" };
   render() {
-    return <div className="flex items-center h-full">{this.state.title}</div>;
+    return (
+      <div className={`flex` + " " + this.props.className}>
+        {this.props.children}
+      </div>
+    );
   }
 }
 
